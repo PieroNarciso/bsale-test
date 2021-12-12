bfdd1851450140e677afe478a37ed0603d0ba9c5
Piero Narciso <pieronarciso988@gmail.com>
2021-12-12 12:53:35 -0500

    Show discount value and new Price

--- client/main.js
+++ client/main.js
@@ -13,6 +13,21 @@ const constructDropdownMenu = () => {
   });
 };
 
+const showDiscountIfExists = (product) => {
+  const newPrice = product.price - (product.price*product.discount / 100);
+  const result = product.discount > 0 ? 
+    `
+      <span class="text-sm text-gray-500 line-through">$ ${product.price.toFixed(2)}</span>
+      <span class="text-lg text-gray-700">$ ${newPrice.toFixed(
+        2
+      )}</span>
+    ` :
+    `<span class="text-lg text-gray-700">$ ${product.price.toFixed(2)}</span>`;
+    
+  
+  return result;
+};
+
 const showProductCards = (query = '', categoryId = null) => {
   const containerElement = document.querySelector('#container-products');
   getProducts(query, categoryId).then((products) => {
@@ -30,9 +45,9 @@ const showProductCards = (query = '', categoryId = null) => {
                   <h5 class="card-title">${product.name.toUpperCase()}</h5>
                   <div class="border-t-2"></div>
                   <div class="card-actions flex justify-between items-center">
-                    <span class="text-lg text-gray-700">$ ${product.price.toFixed(
-                      2
-                    )}</span>
+                  <div class="flex flex-col">
+                    ${showDiscountIfExists(product)}
+                  </div>
                     <button class="btn btn-circle shadow">
                       <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
